4cd1831b8c92c04d682e915b1d48498575905d40
Andrew Eskenazi <drewesk@gmail.com>
2017-06-17 17:49:32 -0600

    Added npm cors.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 
 var app = express();
 
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(cors);
+app.use(cors);
 
 app.use('/api/v1/users', trails)
 
